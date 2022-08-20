72890f00c8fe0da73869710700a3e7f622c20f8f
William Robersone <wmrobersone@yahoo.com>
2022-08-20 04:45:42 -0500

    modal

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -4,6 +4,8 @@ import { baseUrl } from '../constants/movie';
 import { Movie } from '../typings';
 import { FaPlay } from 'react-icons/fa';
 import { InformationCircleIcon } from '@heroicons/react/solid';
+import { modalState, movieState } from '../atoms/modalAtom';
+import { useRecoilState } from 'recoil';
 
 interface Props {
   netflixOriginals: Movie[];
@@ -11,6 +13,8 @@ interface Props {
 
 function Banner({ netflixOriginals }: Props) {
   const [movie, setMovie] = useState<Movie | null>(null);
+  const [showModal, setShowModal] = useRecoilState(modalState);
+  const [currentMovie, setCurrentMovie] = useRecoilState(movieState);
 
   useEffect(() => {
     setMovie(
@@ -18,7 +22,6 @@ function Banner({ netflixOriginals }: Props) {
     );
   }, [netflixOriginals]);
 
-  console.log(movie);
   return (
     <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
       <div className="absolute top-0 left-0 h-[95vh] w-screen -z-10">
@@ -40,7 +43,13 @@ function Banner({ netflixOriginals }: Props) {
         <button className="bannerButton bg-white text-black">
           <FaPlay className="h-4 w-4 text-black md:h-7 md:w-7" /> Play
         </button>
-        <button className="bannerButton bg-[gray]/70">
+        <button
+          className="bannerButton bg-[gray]/70"
+          onClick={() => {
+            setCurrentMovie(movie);
+            setShowModal(true);
+          }}
+        >
           {' '}
           More Info <InformationCircleIcon className="h-5 w-5 md:h-8 md:w-8" />
         </button>
--- components/Header.tsx
+++ components/Header.tsx
@@ -45,7 +45,7 @@ function Header() {
 
       <div className="flex items-center space-x-4 text-sm font-light">
         <SearchIcon className="hidden h-6 w-6 sm:inline" />
-        <p className="hidden lg:inline">William</p>
+        <p className="hidden lg:inline">Kids</p>
         <BellIcon className="h-6 w-6" />
         {/* <Link href="/account"> */}
         <img
